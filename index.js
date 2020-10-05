b80e1fcd7334ef7ffc83ad097a91c1cbf9c838d9
Jannis R <mail@jannisr.de>
2020-10-05 17:46:55 +0200

    rework algorithm, 1.0.1

--- index.js
+++ index.js
@@ -9,7 +9,10 @@ const durationLimits = new WeakMap()
 // This function finds *any* high-enough duration that seems to "touch"
 // the HAFAS-dictated limit of departures() results.
 const findDeparturesDurationLimit = async (hafas, station) => {
-	if (durationLimits.has(hafas)) return durationLimits.get(hafas)
+	if (durationLimits.has(hafas)) {
+		debug('re-using previously determined duration limit')
+		return durationLimits.get(hafas)
+	}
 
 	const testWith = async (duration) => {
 		const opt = {duration, results: Infinity}
@@ -18,25 +21,35 @@ const findDeparturesDurationLimit = async (hafas, station) => {
 		return results
 	}
 
-	let i = 0, lastResults = 0
-	let lower = 15, upper = 30 // minutes
-	while (i++ < MAX_ITERATIONS) {
-		const results = await testWith(upper)
-		if (results <= lastResults) {
-			debug('no increase in results with', upper)
-			break
-		}
-		lastResults = results
-		lower = upper
-		upper *= 2
-	}
+	let i = 0, results = 0, lower = 15, upper = 30 // minutes
 
 	while (i++ < MAX_ITERATIONS) {
-		debug('lower bound', lower, 'upper bound', upper)
-		const middle = Math.round(lower + (upper - lower) / 2)
-		const results = await testWith(middle)
-		if (results > lastResults) lower = duration = middle
-		else upper = middle
+		const newUpper = upper * 2
+		const newResults = await testWith(newUpper)
+
+		if (newResults <= results) {
+			upper *= 1.1
+			debug('no increase in results with', newUpper, `${lower} < limit < ${upper}`)
+			break
+		}
+		results = newResults
+		lower = upper
+		upper = newUpper
+	}
+
+	while (i++ < MAX_ITERATIONS && (upper - lower) > 2) {
+		debug(`${lower} < limit < ${upper}`)
+		const pivot = Math.round(lower + (upper - lower) / 2)
+		const newResults = await testWith(pivot)
+
+		if (newResults < results) {
+			debug('less results')
+			lower = pivot
+		} else { // newResults >= results
+			debug('equal or more results')
+			upper = pivot + 1
+		}
+		results = Math.max(results, newResults)
 	}
 
 	debug('result', lower)
